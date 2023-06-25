c97a60c024b3d99fc35148a67ad86f3958803beb
Manan <mananbansal@gmail.com>
2023-06-25 12:38:31 +0530

    Hashing and Salting use

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB" ,{useNewUrlParser : true});
 
@@ -26,27 +28,34 @@ const User = new mongoose.model("User",userSchema);
 
 app.post("/register",function(req,res){
     
-    const newUser = new User({
-        username : req.body.username,
-        password : md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+        const newUser = new User({
+            username : req.body.username,
+            password : hash
+        });
+    
+        newUser.save().then(function(){
+            res.render("secrets");
+        })
+        .catch(function(err){
+            res.send(err);
+        })  
     });
 
-    newUser.save().then(function(){
-        res.render("secrets");
-    })
-    .catch(function(err){
-        res.send(err);
-    })  
+    
 })
 
 app.post("/login",function(req,res){
     const us = req.body.username;
-    const pw = md5(req.body.password);
+    const pw = req.body.password;
     
     User.findOne({username : us}).then(function(user){
-        if(user.password === pw){
-            res.render("secrets");
-        }
+        bcrypt.compare(pw, user.password).then(function(result) {
+            if(result == true){
+                res.render("secrets");
+            }
+            
+        });
     })
     .catch(function(err){
         console.log(err);
